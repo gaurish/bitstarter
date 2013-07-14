eb8ffa9b543c54d932fb1972c24cc3c9347c280b
Gaurish Sharma <contact@gaurishsharma.com>
2013-07-14 14:58:23 +0530

    Read from index.html

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express')
+  , fs  = require('fs')
   , app = express()
   , port = process.env.PORT || 5000;
 
 app.use(express.logger());
 
+var data = new Buffer(fs.readFileSync('index.html'));
+
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send(data.toString('utf-8'));
 });
 
 app.listen(port, function(){
